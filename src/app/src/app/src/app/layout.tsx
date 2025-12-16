381c4117391f86708509ebb74fd5b856c713694a
QuentinLansival <quentinl_7758l@hotmail.com>
2025-12-16 14:05:05 +0100

    Create layout.tsx

---
+++ src/app/src/app/src/app/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Dispatch MVP"
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
