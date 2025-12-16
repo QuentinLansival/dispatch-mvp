d373a866d841fa2fb6dc419feacb9b9d27ea9558
QuentinLansival <quentinl_7758l@hotmail.com>
2025-12-16 15:14:02 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
+"use client";
+
 import "./globals.css";
 import Nav from "../components/Nav";
 
-export const metadata = {
-  title: "Dispatch MVP",
-};
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="fr">
       <body>
